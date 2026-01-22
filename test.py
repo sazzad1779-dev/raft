422db6e7ff00df5640315068cb30a9014fae52c8
sazzad1779-dev <sazzad1779@gmail.com>
2026-01-22 20:00:09 +0600

    Refactor code structure for improved readability and maintainability

--- test.py
+++
@@ -1,6 +0,0 @@
-from datasets import Dataset
-checkpoints_chunks_path= "results1-checkpoints/questions/checkpoint-0"
-chunks_ds = Dataset.load_from_disk(checkpoints_chunks_path)
-print(chunks_ds)
-for result in chunks_ds:
-    print(f"question: {result["question"]}\ncot_answer: {result["cot_answer"]}\n\n\n")
